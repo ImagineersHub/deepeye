3e94ebda44bd2d7f264de6c6f52e57036ba93e9b
esun <esun@voteb.com>
2024-01-11 23:40:42 +0900

    added test code

---
+++ test_model_name.py
@@ -0,0 +1,8 @@
+from deepeye.models.payload import Payload, ModelType
+
+
+def test_payload_model_name():
+    
+    assert ModelType.BSRGan.name == 'BSRGan'
+
+    assert ModelType.RealESRGan.name == 'RealESRGan'

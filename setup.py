c843c7be2955116c812d2e48656ef8df9b26ed86
esun <esun@voteb.com>
2024-01-12 00:46:46 +0900

    updated pydantic version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='deepeye',
     # Start with a small number and increase it with every change you make
-    version='0.1.7',
+    version='0.1.8',
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     license='MIT',
     # Give a short description about your library
@@ -23,7 +23,7 @@ setup(
         "torchvision",
         "numpy==1.24.2",
         "opencv-contrib-python==4.7.0.72",
-        "pydantic==2.5.3",
+        "pydantic",
         "colorlog",
         "matplotlib"
     ],
--- test_model_name.py
+++ test_model_name.py
@@ -2,7 +2,8 @@ from deepeye.models.payload import Payload, ModelType
 
 
 def test_payload_model_name():
-
+    if not None:
+        print('test')
     assert ModelType.BSRGan.name == 'BSRGan'
 
     assert ModelType.RealESRGan.name == 'RealESRGan'

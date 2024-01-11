f165775b295dfb2a0f43eef8bab2ef06f053dfb8
esun <esun@voteb.com>
2024-01-12 00:02:39 +0900

    update logging

--- deepeye/utils/logging.py
+++ deepeye/utils/logging.py
@@ -1,49 +1,72 @@
 import os
+import sys
 import logging
-from colorlog import ColoredFormatter
 from logging.handlers import TimedRotatingFileHandler
 
+# Optional import for colorlog
+try:
+    from colorlog import ColoredFormatter
+    colorlog_available = True
+except ImportError:
+    colorlog_available = False
 
-def setup_logger(log_folder='logs'):
-    # Ensure log folder exists
-    if not os.path.exists(log_folder):
-        os.makedirs(log_folder)
 
-    # Create a logger
-    logger = logging.getLogger('example_logger')
-    logger.setLevel(logging.DEBUG)
+def setup_logger(logger_name='deepeye', log_level=logging.DEBUG, keep_logs=False, backup_count=5):
+    # Check if logger has already been created to avoid adding multiple handlers
+    logger = logging.getLogger(logger_name)
 
-    # Create a console handler with colored output
+    # If the logger already has handlers, return it as is
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(log_level)
+
+    # Setup console handler
     console_handler = logging.StreamHandler()
-    colored_formatter = ColoredFormatter(
-        "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
-        datefmt=None,
-        reset=True,
-        log_colors={
-            'DEBUG':    'cyan',
-            'INFO':     'green',
-            'WARNING':  'yellow',
-            'ERROR':    'red',
-            'CRITICAL': 'red,bg_white',
-        },
-        secondary_log_colors={},
-        style='%'
-    )
-    console_handler.setFormatter(colored_formatter)
+    if colorlog_available:
+        colored_formatter = ColoredFormatter(
+            "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+            datefmt=None,
+            reset=True,
+            log_colors={
+                'DEBUG':    'cyan',
+                'INFO':     'green',
+                'WARNING':  'yellow',
+                'ERROR':    'red',
+                'CRITICAL': 'red,bg_white',
+            },
+            style='%'
+        )
+        console_handler.setFormatter(colored_formatter)
+    else:
+        console_formatter = logging.Formatter('%(levelname)-8s %(message)s')
+        console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
 
-    # Create a file handler that logs messages to a file in the specified folder, rotating daily
-    log_file_path = os.path.join(log_folder, 'daily_log')
-    file_handler = TimedRotatingFileHandler(
-        log_file_path, when="midnight", interval=1, backupCount=5)
-    file_handler.suffix = "%Y-%m-%d"
-    file_formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(file_formatter)
-    logger.addHandler(file_handler)
+    # File handler setup
+    if keep_logs:
+        try:
+            # Ensure log folder exists
+            if log_folder is None:
+                base_dir = os.path.dirname(os.path.abspath(
+                    sys.modules['__main__'].__file__))
+                log_folder = os.path.join(base_dir, 'logs')
+
+            # Create a file handler that logs messages to a file, rotating daily
+            log_file_path = os.path.join(log_folder, logger_name + '.log')
+            file_handler = TimedRotatingFileHandler(
+                log_file_path, when="midnight", interval=1, backupCount=backup_count)
+            file_handler.suffix = "%Y-%m-%d"
+            file_formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            file_handler.setFormatter(file_formatter)
+            logger.addHandler(file_handler)
+
+        except Exception as e:
+            logger.error("Failed to create file handler: %s", e)
 
     return logger
 
 
-# Setup the logger
+# Example usage in any script
 logger = setup_logger()

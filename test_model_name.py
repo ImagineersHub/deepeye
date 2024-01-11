5aaf4e81bd037236fdc94925e921c7c4a1560f46
esun <esun@voteb.com>
2024-01-12 00:03:48 +0900

    updated test code

--- test_model_name.py
+++ test_model_name.py
@@ -1,5 +1,4 @@
 from deepeye.models.payload import Payload, ModelType
-from deepeye.srgan_wrapper import BSRGan, RealESRGan
 
 
 def test_payload_model_name():
---
+++ unitest.py
@@ -0,0 +1,1 @@
+from deepeye.srgan_wrapper import BSRGan, RealESRGan
